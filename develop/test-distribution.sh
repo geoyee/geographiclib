ed5d1168505b5c0a56b5d9cc2ee78a1cf63a3df5
Charles Karney <karney@alum.mit.edu>
2025-11-07 16:23:04 -0500

    Start new version on devel branch

--- develop/test-distribution.sh
+++ develop/test-distribution.sh
@@ -36,10 +36,10 @@ umask 0022
 
 START=`date +%s`
 DATE=`date +%F`
-VERSION=2.7
-SUFFIX=
+VERSION=2.8
+SUFFIX=-alpha
 DISTVERSION=$VERSION$SUFFIX
-BRANCH=main
+BRANCH=devel
 TEMP=/home/scratch/geographiclib-dist
 if test `hostname` = petrel; then
     DEVELSOURCE=$HOME/geographiclib
